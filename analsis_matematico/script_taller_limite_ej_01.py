f0aa23d058e169e7f10f79d434bc52b3ad4913a6
Paola <paotorres89@gmail.com>
2023-04-25 12:57:07 -0300

    agregado otra opcion de generar valores

--- analsis_matematico/script_taller_limite_ej_01.py
+++ analsis_matematico/script_taller_limite_ej_01.py
@@ -17,6 +17,12 @@ def l(v):
 
 v = np.array([2.99999997E+8,2.99999998E+8,2.99999999E+8])
 
+#Para crear n cantidad de valores se puede comentar la linea anterior (agregando # al inicio de la inea) y descomentar las siguientes lineas
+#start = 1 # Este es el valor donde quieren iniciar la tabla
+#stop = 1e8 #Este es el valor final de la tabla 
+#n_valores #esta es la cantidad de valores que quieren
+#v = np.linspace (start,stop, num=n_valores)
+
 print(l(v))     
 
 tabla = pd.DataFrame( list(zip(v, l(v))), columns=['v', 'l'] )
